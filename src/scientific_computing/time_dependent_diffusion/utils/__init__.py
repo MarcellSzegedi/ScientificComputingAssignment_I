03ae38df5ddfd568740539533c8d91715591c0f7
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-02-12 20:28:08 +0100

    restrict test to parameters yielding stable scheme

--- src/scientific_computing/time_dependent_diffusion/utils/__init__.py
+++ src/scientific_computing/time_dependent_diffusion/utils/__init__.py
@@ -1,3 +1,6 @@
 from .time_dependent_diffusion import (
+    is_stable_scheme as is_stable_scheme,
+)
+from .time_dependent_diffusion import (
     time_dependent_diffusion as time_dependent_diffusion,
 )
