52a2bc76e4477e5a81d861d7d40e2718f1427ab9
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-02-16 12:08:38 +0100

    separate numba version, write test to check numba version gives same result

--- src/scientific_computing/time_dependent_diffusion/utils/time_dependent_diffusion.py
+++ src/scientific_computing/time_dependent_diffusion/utils/time_dependent_diffusion.py
@@ -6,7 +6,7 @@ from scipy.special import erfc
 
 
 @njit
-def one_step_diffusion(
+def one_step_diffusion_numba(
     grid: npt.NDArray[np.float64],
     buffer: npt.NDArray[np.float64],
     dt: float,
@@ -33,8 +33,9 @@ def one_step_diffusion(
     return grid
 
 
-# @njit
-def time_dependent_diffusion(time_steps: int, intervals: int, dt: float, D: float):
+def time_dependent_diffusion_numba(
+    time_steps: int, intervals: int, dt: float, D: float
+):
     if time_steps < 1:
         raise ValueError("Time steps must be greater than 2.")
 
@@ -45,7 +46,41 @@ def time_dependent_diffusion(time_steps: int, intervals: int, dt: float, D: floa
 
     buffer = np.zeros((intervals, intervals), dtype=np.float64)
     for _ in range(1, time_steps):
-        grid = one_step_diffusion(grid, buffer, dt, dx, D)
+        grid = one_step_diffusion_numba(grid, buffer, dt, dx, D)
+        grid_history.append(grid)
+
+    return grid, grid_history
+
+
+def one_step_diffusion(grid: npt.NDArray[np.float64], dt: float, dx: float, D: float):
+    new_grid = grid.copy()
+    diffusion_coeff = (dt * D) / (dx**2)
+    for i in range(1, grid.shape[0] - 1):
+        for j in range(0, grid.shape[1]):
+            neighbor_concentration_diff = (
+                grid[i + 1, j]  # Lower neighbor
+                + grid[i - 1, j]  # Upper neighbor
+                + grid[i, (j + 1) % grid.shape[1]]  # Right neighbor
+                + grid[i, (j - 1) % grid.shape[1]]  # Left neighbor
+                - 4 * grid[i, j]  # Self
+            )
+
+            new_grid[i, j] = grid[i, j] + diffusion_coeff * neighbor_concentration_diff
+            # grid[i, j] = new_grid[i, j]
+    return new_grid
+
+
+def time_dependent_diffusion(time_steps: int, intervals: int, dt: float, D: float):
+    if time_steps < 1:
+        raise ValueError("Time steps must be greater than 2.")
+
+    grid_history = []
+    grid = np.zeros((intervals, intervals), dtype=np.float64)
+    grid[0] = 1
+    dx = 1 / intervals
+
+    for _ in range(1, time_steps):
+        grid = one_step_diffusion(grid, dt, dx, D)
         grid_history.append(grid)
 
     return grid, grid_history
--- tests/time_dependent_diffusion/test_time_dependent_diffusion_utils.py
+++ tests/time_dependent_diffusion/test_time_dependent_diffusion_utils.py
@@ -1,9 +1,11 @@
+import numpy as np
 import pytest
 from hypothesis import given, settings
 from hypothesis.strategies import floats, integers
 
 from scientific_computing.time_dependent_diffusion.utils import (
     time_dependent_diffusion,
+    time_dependent_diffusion_numba,
 )
 
 
@@ -17,6 +19,20 @@ from scientific_computing.time_dependent_diffusion.utils import (
 def test_grid_shape(time_steps, intervals, dt, D):
     grid, _ = time_dependent_diffusion(time_steps, intervals, dt, D)
     assert grid.shape == (intervals, intervals)
+
+
+@settings(deadline=None)
+@given(
+    intervals=integers(min_value=1, max_value=10),
+    time_steps=integers(min_value=1, max_value=1000),
+    dt=floats(min_value=0, max_value=1, allow_nan=False, allow_infinity=False),
+    D=floats(min_value=0, allow_nan=False, allow_infinity=False),
+)
+def test_numba_version_gives_same_results(time_steps, intervals, dt, D):
+    grid_python, _ = time_dependent_diffusion(time_steps, intervals, dt, D)
+    grid_numba, _ = time_dependent_diffusion_numba(time_steps, intervals, dt, D)
+
+    np.testing.assert_allclose(grid_python, grid_numba)
 
 
 def test_diffusion_time_steps_and_intervals_less_than_one():
