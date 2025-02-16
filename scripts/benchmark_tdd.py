1a90c153d48e10a9b0f6bb9be966bcc0b585d418
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-02-16 12:08:38 +0100

    separate numba version, write test to check numba version gives same result

--- scripts/benchmark_tdd.py
+++ scripts/benchmark_tdd.py
@@ -1,7 +1,7 @@
 from cProfile import Profile
 from pstats import SortKey, Stats
 
-from scientific_computing.time_dependent_diffusion import time_dependent_diffusion
+from scientific_computing.time_dependent_diffusion import time_dependent_diffusion_numba
 
 if __name__ == "__main__":
     ITERATIONS = 5
@@ -12,5 +12,5 @@ if __name__ == "__main__":
 
     with Profile() as profile:
         for _ in range(ITERATIONS):
-            grid = time_dependent_diffusion(TIME_STEPS, INTERVALS, DT, D)
+            grid = time_dependent_diffusion_numba(TIME_STEPS, INTERVALS, DT, D)
         Stats(profile).strip_dirs().sort_stats(SortKey.TIME).print_stats(25)
--- tests/time_dependent_diffusion/test_time_dependent_diffusion_utils.py
+++ tests/time_dependent_diffusion/test_time_dependent_diffusion_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from hypothesis import assume, given, settings
 from hypothesis.strategies import floats, integers
@@ -5,6 +6,7 @@ from hypothesis.strategies import floats, integers
 from scientific_computing.time_dependent_diffusion.utils import (
     is_stable_scheme,
     time_dependent_diffusion,
+    time_dependent_diffusion_numba,
 )
 
 
@@ -28,6 +30,20 @@ def test_grid_shape(time_steps, intervals, dt, D):
 )
 def test_is_stable_scheme(intervals, dt, D):
     is_stable_scheme(dt, 1 / intervals, D)
+
+
+@settings(deadline=None)
+@given(
+    intervals=integers(min_value=1, max_value=10),
+    time_steps=integers(min_value=1, max_value=1000),
+    dt=floats(min_value=0, max_value=1, allow_nan=False, allow_infinity=False),
+    D=floats(min_value=0, allow_nan=False, allow_infinity=False),
+)
+def test_numba_version_gives_same_results(time_steps, intervals, dt, D):
+    grid_python, _ = time_dependent_diffusion(time_steps, intervals, dt, D)
+    grid_numba, _ = time_dependent_diffusion_numba(time_steps, intervals, dt, D)
+
+    np.testing.assert_allclose(grid_python, grid_numba)
 
 
 def test_diffusion_time_steps_and_intervals_less_than_one():
