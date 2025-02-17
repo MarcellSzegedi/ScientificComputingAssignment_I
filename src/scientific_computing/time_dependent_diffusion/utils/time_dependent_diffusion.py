c963a95ff2f47cd9fb1134dd7a110ef2993b6420
Henry Zwart <henry.zwart@dragonfly.co.nz>
2025-02-17 20:05:17 +0100

    move time-dependent diffusion into class to keep organised

--- src/scientific_computing/time_dependent_diffusion/utils/time_dependent_diffusion.py
+++ src/scientific_computing/time_dependent_diffusion/utils/time_dependent_diffusion.py
@@ -1,8 +1,83 @@
+from enum import StrEnum
+
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 from numba import njit
 from scipy.special import erfc
+
+from scientific_computing._core import td_diffusion_cylinder
+
+
+class RunMode(StrEnum):
+    Python = "python"
+    Numba = "numba"
+    Rust = "rust"
+
+
+class Cylinder:
+    def __init__(self, spatial_intervals: int, diffusivity: float):
+        self.grid = np.zeros((spatial_intervals, spatial_intervals), dtype=np.float64)
+        self.grid[0] = 1.0
+        self.dx = 1 / spatial_intervals
+        self.diffusivity = diffusivity
+
+    def run(self, n_iters: int, dt: float, mode: RunMode = RunMode.Python):
+        if n_iters < 0:
+            raise ValueError("n_iters must be positive.")
+
+        match mode:
+            case RunMode.Python:
+                update = one_step_diffusion
+            case RunMode.Numba:
+                update = one_step_diffusion_numba
+            case RunMode.Rust:
+                self.grid = td_diffusion_cylinder(
+                    [n_iters],
+                    intervals=self.grid.shape[0],
+                    dt=dt,
+                    diffusivity=self.diffusivity,
+                )[-1]
+                return
+
+        buffer = np.zeros_like(self.grid)
+        for _ in range(n_iters):
+            self.grid = update(self.grid, buffer, dt, self.dx, self.diffusivity)
+
+    def measure(
+        self, measurement_times: list[float], dt: float, mode: RunMode = RunMode.Python
+    ):
+        if len(measurement_times) == 0:
+            raise ValueError("Must specify at least one measurement time.")
+        elif min(measurement_times) < 0.0 or max(measurement_times) > 1.0:
+            raise ValueError("Measurement times must be in interval [0,1]")
+        measurement_idxes = set(int(t / dt) for t in measurement_times)
+
+        match mode:
+            case RunMode.Python:
+                update = one_step_diffusion
+            case RunMode.Numba:
+                update = one_step_diffusion_numba
+            case RunMode.Rust:
+                measurements = td_diffusion_cylinder(
+                    list(measurement_idxes),
+                    intervals=self.grid.shape[0],
+                    dt=dt,
+                    diffusivity=self.diffusivity,
+                )
+                self.grid = measurements[-1].copy()
+                return measurements
+
+        measurements = []
+        time_steps = max(measurement_idxes)
+
+        buffer = np.zeros_like(self.grid)
+        for t in range(time_steps + 1):
+            if t in measurement_idxes:
+                measurements.append(self.grid.copy())
+            self.grid = update(self.grid, buffer, dt, self.dx, self.diffusivity)
+
+        return measurements
 
 
 @njit
@@ -33,27 +108,13 @@ def one_step_diffusion_numba(
     return grid
 
 
-def time_dependent_diffusion_numba(
-    time_steps: int, intervals: int, dt: float, D: float
+def one_step_diffusion(
+    grid: npt.NDArray[np.float64],
+    buffer: npt.NDArray[np.float64],
+    dt: float,
+    dx: float,
+    D: float,
 ):
-    if time_steps < 1:
-        raise ValueError("Time steps must be greater than 2.")
-
-    grid = np.zeros((intervals, intervals), dtype=np.float64)
-    grid[0] = 1
-    dx = 1 / intervals
-
-    buffer = np.zeros((intervals, intervals), dtype=np.float64)
-    grid_history = [grid.copy()]
-    for _ in range(time_steps):
-        grid = one_step_diffusion_numba(grid, buffer, dt, dx, D)
-        grid_history.append(grid.copy())
-
-    return grid, grid_history
-
-
-def one_step_diffusion(grid: npt.NDArray[np.float64], dt: float, dx: float, D: float):
-    new_grid = grid.copy()
     diffusion_coeff = (dt * D) / (dx**2)
     for i in range(1, grid.shape[0] - 1):
         for j in range(0, grid.shape[1]):
@@ -64,26 +125,10 @@ def one_step_diffusion(grid: npt.NDArray[np.float64], dt: float, dx: float, D: f
                 + grid[i, (j - 1) % grid.shape[1]]  # Left neighbor
                 - 4 * grid[i, j]  # Self
             )
+            buffer[i, j] = grid[i, j] + diffusion_coeff * neighbor_concentration_diff
 
-            new_grid[i, j] = grid[i, j] + diffusion_coeff * neighbor_concentration_diff
-            # grid[i, j] = new_grid[i, j]
-    return new_grid
-
-
-def time_dependent_diffusion(time_steps: int, intervals: int, dt: float, D: float):
-    if time_steps < 1:
-        raise ValueError("Time steps must be greater than 2.")
-
-    grid = np.zeros((intervals, intervals), dtype=np.float64)
-    grid[0] = 1
-    dx = 1 / intervals
-
-    grid_history = [grid.copy()]
-    for _ in range(time_steps):
-        grid = one_step_diffusion(grid, dt, dx, D)
-        grid_history.append(grid.copy())
-
-    return grid, grid_history
+    grid[1:-1, ...] = buffer[1:-1, ...]
+    return grid
 
 
 def analytical_solution(y: float, D: float, t: float, terms: int):
@@ -95,24 +140,26 @@ def analytical_solution(y: float, D: float, t: float, terms: int):
     return solution
 
 
-def plot_solution_comparison(dt: float, time_steps: int, intervals: int, terms: int):
-    t_range = [0.001, 0.01, 0.1, 1.0]
+def plot_solution_comparison(
+    dt: float,
+    measurement_times: list[float],
+    intervals: int,
+    terms: int,
+    mode: RunMode = RunMode.Python,
+):
     y_range = np.linspace(0, 1, intervals)
     D = 1
 
     fig, axes = plt.subplots(2, 2, figsize=(10, 10))
     axes = axes.flatten()
 
-    simulation_result, grid_history = time_dependent_diffusion(
-        time_steps, intervals, dt, D
+    cylinder = Cylinder(spatial_intervals=intervals, diffusivity=D)
+    measurements = cylinder.measure(
+        measurement_times=measurement_times, dt=dt, mode=mode
     )
 
-    for idx, t in enumerate(t_range):
-        time_step_idx = int(t / dt)
-        if time_step_idx >= len(grid_history):
-            grid_at_t = grid_history[-1]
-        else:
-            grid_at_t = grid_history[time_step_idx]
+    for idx, t in enumerate(measurement_times):
+        grid_at_t = measurements[idx]
         simulation_result_y = grid_at_t[::-1, 0]
         analytical_result_y = [analytical_solution(y, D, t, terms) for y in y_range]
 
